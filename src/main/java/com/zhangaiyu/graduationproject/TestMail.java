143e056f2d8ee5431b6f99dc8a926fbe6cbfe49e
毕业设计 <zhangaiyu911@163.com>
2019-01-03 16:18:39 +0800

    简单修改

--- src/main/java/com/zhangaiyu/graduationproject/TestMail.java
+++ src/main/java/com/zhangaiyu/graduationproject/TestMail.java
@@ -8,12 +8,12 @@ import java.util.ArrayList;
 public class TestMail {
     public static void main(String[] args)throws Exception{
         new MailSender()
-                .title("测试springboot发送邮件")
-                .content("简单文本内容发送")
+                .title("递给刘凯的一封信")
+                .content("刘凯刘凯刘凯")
                 .contentType(MailContentTypeEnum.TEXT)
                 .targets(new ArrayList<String>(){
                     {
-                        add("zhangaiyu911@163.com");
+                        add("885721977@qq.com");
                     }
                 })
                 .send();
