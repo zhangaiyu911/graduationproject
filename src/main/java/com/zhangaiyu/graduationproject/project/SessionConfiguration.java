b5a50abb885f1048d0cebefa29e5b6d98b967224
毕业设计 <zhangaiyu911@163.com>
2019-01-03 14:48:39 +0800

    Create SessionConfiguration.java
    配置session

---
+++ src/main/java/com/zhangaiyu/graduationproject/project/SessionConfiguration.java
@@ -0,0 +1,15 @@
+package com.zhangaiyu.graduationproject.project;
+
+import com.zhangaiyu.graduationproject.project.interceptor.SessionInterceptor;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.web.servlet.config.annotation.InterceptorRegistry;
+import org.springframework.web.servlet.config.annotation.WebMvcConfigurerAdapter;
+
+@Configuration
+public class SessionConfiguration extends WebMvcConfigurerAdapter {
+
+    @Override
+    public void addInterceptors(InterceptorRegistry registry) {
+        registry.addInterceptor(new SessionInterceptor()).addPathPatterns("/**");
+    }
+}
