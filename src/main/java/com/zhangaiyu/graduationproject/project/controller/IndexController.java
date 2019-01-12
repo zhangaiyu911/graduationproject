83740bd635f06cb65c9e50daa33f7f9c04fe6e29
毕业设计 <zhangaiyu911@163.com>
2019-01-12 08:32:24 +0800

    Update IndexController.java

--- src/main/java/com/zhangaiyu/graduationproject/project/controller/IndexController.java
+++ src/main/java/com/zhangaiyu/graduationproject/project/controller/IndexController.java
@@ -1,23 +1,29 @@
 package com.zhangaiyu.graduationproject.project.controller;
 
+import com.alibaba.fastjson.JSONObject;
+import com.zhangaiyu.graduationproject.project.utils.LoggerUtils;
 import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestMethod;
 
+import javax.servlet.http.HttpServletRequest;
+
 @Controller
-@RequestMapping(value = "/user")
+@RequestMapping(value = "/index")
 public class IndexController {
 
     /**
      * 初始化登录界面
      */
-    @RequestMapping(value = "/login_view",method = RequestMethod.GET)
-    public String login_view(){
-        return "login";
+    @RequestMapping(value = "/login",method = RequestMethod.GET)
+    public JSONObject login(HttpServletRequest request,String name) throws Exception{
+
+        JSONObject obj = new JSONObject();
+        obj.put("msg","用户:"+name+",登录成功。");
+        //将返回值写入到请求对象中
+        request.setAttribute(LoggerUtils.LOGGER_RETURN,obj);
+        return obj;
+
     }
 
-    @RequestMapping(value = "/index",method = RequestMethod.GET)
-    public String index(){
-        return "index";
-    }
 }
