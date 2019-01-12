a5acb359d60bc48289d534522caca0f563581264
毕业设计 <zhangaiyu911@163.com>
2019-01-12 08:32:52 +0800

    Create LoggerConfiguration.java

---
+++ src/main/java/com/zhangaiyu/graduationproject/LoggerConfiguration.java
@@ -0,0 +1,15 @@
+package com.zhangaiyu.graduationproject;
+
+import com.zhangaiyu.graduationproject.project.interceptor.LoggerInterceptor;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.web.servlet.config.annotation.InterceptorRegistry;
+import org.springframework.web.servlet.config.annotation.WebMvcConfigurerAdapter;
+
+@Configuration
+public class LoggerConfiguration extends WebMvcConfigurerAdapter {
+
+    @Override
+    public void addInterceptors(InterceptorRegistry registry) {
+        registry.addInterceptor(new LoggerInterceptor()).addPathPatterns("/**");
+    }
+}
